ac9561b3872e660e10f777176a04c31b3e2a72bd
Eriyc <eric@nodeiwest.se>
2021-04-03 02:28:37 +0200

    blog-inlägg loggas nu till consolen

---
+++ js/blog_posts.js
@@ -0,0 +1,22 @@
+const createArticleHTML = (article) => {
+  const { title, summary, author, published } = article;
+
+  const date = new Date(published);
+
+  return `<article>
+    <h3>${title}</h3>
+    <h4>${author} - ${date.toDateString()}<h4>
+    <p>${summary}</p>
+  </article>`;
+};
+
+const blogContainer = document.getElementById("blog");
+const fetchLatestBlogPosts = async () => {
+  const posts = await fetch("https://blog.dgren.dev/api/latest").then((res) =>
+    res.json()
+  );
+
+  console.log(posts);
+};
+
+fetchLatestBlogPosts();
